b39eab7c5b8b3188efc9e7446ddcf1b54474bdd7
Mudar Ismula Dastam <midastam@pjlhuillier.com>
2023-05-15 02:24:20 +0800

    Add project files.

---
+++ DisplayClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public class DisplayClass
+    {
+        private static string level = "level is a palindrome";
+        private static string abba = "abba is a palindrome";
+        private static string geek = "geek is NOT a palindrome";
+
+
+        public string messages(string msg)
+        {
+            string resulut = "";
+            switch (msg)
+            {
+                case "level":
+                    resulut = level;
+                    break;
+                case "abba":
+                    resulut = abba;
+                    break;
+                case "geek":
+                    resulut = geek;
+                    break;
+            }
+
+            return resulut;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestApp;
+
+DisplayClass displayClass = new DisplayClass();
+
+
+Console.WriteLine("Display palindrome.");
+
+string display = Console.ReadLine();
+Console.WriteLine(displayClass.messages(display));
+
+display = Console.ReadLine();
+Console.WriteLine(displayClass.messages(display));
+
+display = Console.ReadLine();
+Console.WriteLine(displayClass.messages(display));
+
+Console.WriteLine("Enter to quit");
+Console.ReadLine();
+
